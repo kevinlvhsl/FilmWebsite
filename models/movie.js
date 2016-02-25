1477298e6d97290b481e3ec072366120375e021b
kevinlvhsl <353030799@qq.com>
2016-02-25 17:11:29 +0800

    Update movie.js

--- models/movie.js
+++ models/movie.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
-var MovieSchema = require('../schemas/movie.js')
+var MovieSchema = require('../schemas/movie')
 //将模式编译成 movie模型 model
 var Movie = mongoose.model('Movie', MovieSchema)
 
