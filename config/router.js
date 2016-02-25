052486d038259efb3dc10578f3d89bbf15857192
kevinlvhsl <353030799@qq.com>
2016-02-25 17:28:45 +0800

    update router user

--- config/router.js
+++ config/router.js
@@ -1,4 +1,5 @@
 var Movie = require('./app/models/movie')
+var User = require('./app/models/user')
 
 //express route
 app.get('/',function(req, res){
@@ -52,75 +53,15 @@ app.get('/admin/update/:id', function(req, res){
 })
 
 //admin post movie 提交请求处理
-app.post('/admin/movie/new', function(req, res){
-	console.log(req.body)
-	var id = req.body.movie._id, 
-		movieObj = req.body.movie, 
-		_movie = null
-	if (id !== 'undefined') {
-		Movie.findById(id, function(err, movie){
-			if (err) {
-				console.log(err)
-			}
-			//underscore 将提交来的对象覆盖数据库中查询到的数据
-			_movie = _.extend(movie, movieObj)
-			_movie.save(function(err, movie){
-			if (err) {
-					console.log(err)
-				}
-				res.redirect('/movie'+ movie._id)
-			})
-		})
-	}else{
-		console.log('meiyou id'+ movieObj)
-		_movie = new Movie({
-			doctor : movieObj.doctor,
-			title : movieObj.title,
-			language : movieObj.language,
-			country : movieObj.country,
-			summary : movieObj.summary,
-			poster : movieObj.poster,
-			flash : movieObj.flash,
-			year : movieObj.year,
-			createAt: Date.now()
-		})
-		//将表单数据重新保存到数据库，并且页面重定向到详情页
-		_movie.save(function(err, movie){
-			if (err) {
-				console.log(err)
-			}
-			res.redirect('/movie/'+ movie._id)
-		})
-	}
-})
+app.post('/admin/movie/new', Movie.new)
  
 //express route
-app.get('/admin/list',function(req, res){
-	Movie.fetch(function(err, movies){
-		if (err) {
-			console.log(err)
-		}
-		//渲染到页面
-		res.render('list' ,{title: 'kevin 列表页',
-			movies: movies
-		})
-	})
-	
-})
+app.get('/admin/list',Movie.list)
 
 
 //list delete movie
-app.delete('/admin/list', function(req, res){
-	var id= req.query.id
-	if (id) {
-		Movie.remove({_id: id}, function(err, movie){
-			if (err) {
-				console.log(err)
-			}
-			else{
-				res.json({success: 1})
-			}
+app.delete('/admin/:id', Movie.del)
 
-		})
-	}
-})
+
+// user
+app.get("/user/list", User.list)
